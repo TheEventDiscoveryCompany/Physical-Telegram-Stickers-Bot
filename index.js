3f5cf69f6b87fc4a243877e08df81adf261c484c
Andrew Jarvis <ajarvis7771@gmail.com>
2018-01-09 20:42:48 -0500

    another log for heroku

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.post('/d7bac4ef-9b4d-47c8-ad47-c33f0e4a5561', function(req, res) {
     }
 
     console.log(update);
+    console.log(entities);
 
     axios.post(telegramApiUrl + 'sendMessage', {
         chat_id: update.message.chat.id,
