109320dae516dc2785e531a0360ae508bb34c9c1
Andrew Jarvis <ajarvis7771@gmail.com>
2018-01-09 19:41:36 -0500

    message responses

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Physical-Telegram-Stickers
 🅱️🅱️🅱️🅱️🅱️🅱️🅱️🅱️🅱️
+
+
+## Setting Bot Webhook
+This bot uses webhooks to retrieve Telegram messages. The webhook needs to be set on Telegram before receiving messages.
+
+To set the webhook:
+```bash
+curl -F "url=https://example.com/new-message-endpoint"  https://api.telegram.org/bot<api_token>/setWebhook
+```
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 var axios = require('axios');
 
 var port = process.env.PORT || 3000;
+var telegramApiUrl = "https://api.telegram.org/bot" + process.env.TELEGRAM_BOT_TOKEN + "/";
 
 // Use a GUID for the URL to eliminate fake messages
 app.post('/d7bac4ef-9b4d-47c8-ad47-c33f0e4a5561', function(req, res) {
@@ -15,7 +16,23 @@ app.post('/d7bac4ef-9b4d-47c8-ad47-c33f0e4a5561', function(req, res) {
         return res.end();
     }
 
+    axios.post(telegramApiUrl + 'sendMessage', {
+        chat_id: message.chat.id,
+        text: '🅱️ello 🅱️orld'
+      })
+        .then(response => {
+            console.log('Message Sent');
+            res.end('ok');
+        })
+        .catch(err => {
+            // ...and here if it was not
+            console.log('Error :', err);
+            res.end('Error :' + err);
+        });    
+
 });
+
+
 
 app.get('/', function(req, res) {
     res.send("To be fair, you have to have a very high IQ to understand Rick and Morty. The humour is extremely subtle, and without a solid grasp of theoretical physics most of the jokes will go over a typical viewers head. There's also Rick's nihilistic outlook, which is deftly woven into his characterisation- his personal philosophy draws heavily from Narodnaya Volya literature, for instance. The fans understand this stuff; they have the intellectual capacity to truly appreciate the depths of these jokes, to realise that they're not just funny- they say something deep about LIFE. As a consequence people who dislike Rick & Morty truly ARE idiots- of course they wouldn't appreciate, for instance, the humour in Rick's existential catchphrase \"Wubba Lubba Dub Dub,\" which itself is a cryptic reference to Turgenevs Russian epic Fathers and Sons. I'm smirking right now just imagining one of those addlepated simpletons scratching their heads in confusion as Dan Harmon's genius wit unfolds itself on their television screens. What fools.. how I pity them. 😂<br><br>And yes, by the way, i DO have a Rick & Morty tattoo. And no, you cannot see it. It's for the ladies' eyes only- and even then they have to demonstrate that they're within 5 IQ points of my own (preferably lower) beforehand. Nothin personnel kid 😎");
