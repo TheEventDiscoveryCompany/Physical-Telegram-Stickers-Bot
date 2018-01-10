12acbad751296e7f3a9529431eb15bf9cdfcb10a
Andrew Jarvis <ajarvis7771@gmail.com>
2018-01-09 20:16:45 -0500

    console.log for debugging

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ app.post('/d7bac4ef-9b4d-47c8-ad47-c33f0e4a5561', function(req, res) {
         return res.end();
     }
 
+    console.log(message);
+
     axios.post(telegramApiUrl + 'sendMessage', {
         chat_id: message.chat.id,
         text: '🅱️ello 🅱️orld'
